39b6ec61b609d7d4061cff0127abba4ba2ea9ecc
Joe Stelmach <joestelmach@gmail.com>
2010-02-11 14:47:58 -0500

    sinatra app now sends back errors

--- src/main/resources/web/app.rb
+++ src/main/resources/web/app.rb
@@ -10,10 +10,11 @@ post '/parse' do
   puts 'parsing'
 
   # invoke the parser with the given input
-  output = ""
+  output = ''
   IO.popen("java -jar parser.jar '#{params[:value]}'") do |parser|
     parser.each{|line| output << line} 
   end
+  output = "{\"errors\":\"#{output.strip()}\"}" if (("{" =~ /^\{/) == 0)
 
   content_type :json
   output
