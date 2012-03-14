e80bff088e7e1a040fa51d2cb622190b385dce5b
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-03-14 23:28:20 +0300

    Make parser thread safe.

--- src/main/java/com/joestelmach/natty/CalendarSource.java
+++ src/main/java/com/joestelmach/natty/CalendarSource.java
@@ -6,22 +6,22 @@ import java.util.GregorianCalendar;
 /**
  * Responsible for generating new Calendars that represent
  * the current point in time.  This is neccessary so we can
- * manipulate what the software thinks is the 'current' 
+ * manipulate what the software thinks is the 'current'
  * time, which may be different than the system time
- * 
+ *
  * @author Joe Stelmach
  */
-class CalendarSource {
-  private static Date _baseDate;
-  
+public class CalendarSource {
+  private static ThreadLocal<Date> _baseDate = new ThreadLocal<Date>();
+
   public static void setBaseDate(Date baseDate) {
-    _baseDate = baseDate;
+    _baseDate.set(baseDate);
   }
 
   public static GregorianCalendar getCurrentCalendar() {
     GregorianCalendar calendar = new GregorianCalendar();
-    if(_baseDate != null) {
-      calendar.setTime(_baseDate);
+    if(_baseDate.get() != null) {
+      calendar.setTime(_baseDate.get());
     }
     return calendar;
   }
